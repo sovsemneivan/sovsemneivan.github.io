a4c527abf2327713372a96b047845b36ee734f3f
sovsemneivan.github.io <ivan123456f@gmail.com>
2018-05-24 16:42:18 +0300

    1

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 
-        var gameobject;
-        var myObstacles = [];
-        var myScore;
+        var gameobject;//это куб
+        var myObstacles = [];// это столбы
+        var myScore;// это счет
 
-        function startGame() {
-            gameobject = new component(15, 15, "circle.png", 10, 120, "image");
-            gameobject.gravity = 0.5;
-            myScore = new component("30px", "Consolas", "black", 280, 40, "text");
-            myGameArea.start();
+        function startGame() { // начало скрипта
+            gameobject = new component(15, 15, "circle.png", 10, 120, "image"); // введение ключевого объекта
+            gameobject.gravity = 0.2; // то, как быстро объект падает вниз сначала
+            myScore = new component("30px", "Consolas", "black", 280, 40, "text"); // окно счета
+            myGameArea.start(); // старт
         }
 
-        var myGameArea = {
+        var myGameArea = { // подключение канваса
             canvas: document.createElement("canvas"),
             start: function() {
-                this.canvas.width = 480;
-                this.canvas.height = 270;
+                this.canvas.width = 700;////this это способ подключения к объекту
+                this.canvas.height = 700;
                 this.context = this.canvas.getContext("2d");
-                document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+                document.body.insertBefore(this.canvas, document.body.childNodes[0]); ////childNodes это элементы апи(програмного интерфейса)
                 this.frameNo = 0;
-                this.interval = setInterval(updateGameArea, 20);
+                this.interval = setInterval(updateGameArea, 20);//очистка пространства для столбов
             },
             clear: function() {
                 this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
             }
         }
 
-        function component(width, height, color, x, y, type) {
+        function component(width, height, color, x, y, type) { //функция width, height, color, x, y, type и их свойства
             this.type = type;
             this.score = 0;
             this.width = width;
             this.height = height;
-            this.speedX = 1;
+            this.speedX = 0;
             this.speedY = 0;
             this.x = x;
             this.y = y;
             this.gravity = 0.5;
             this.gravitySpeed = 0;
             this.update = function() {
-                ctx = myGameArea.context;
+                context = myGameArea.context;
                 if (this.type == "text") {
-                    ctx.font = this.width + " " + this.height;
-                    ctx.fillStyle = color;
-                    ctx.fillText(this.text, this.x, this.y);
+                   context.font = this.width + " " + this.height;
+                    context.fillStyle = color;
+                    context.fillText(this.text, this.x, this.y);
                 } else {
-                    ctx.fillStyle = color;
-                    ctx.fillRect(this.x, this.y, this.width, this.height);
+                    context.fillStyle = color;
+                    context.fillRect(this.x, this.y, this.width, this.height);
                 }
             }
-            this.newPos = function() {
+            this.newPos = function() {/// действия, если объект каасется дна
                 this.gravitySpeed += this.gravity;
                 this.x += this.speedX;
                 this.y += this.speedY + this.gravitySpeed;
@@ -57,7 +57,7 @@
                 var rockbottom = myGameArea.canvas.height - this.height;
                 if (this.y > rockbottom) {
                     this.y = rockbottom;
-                    this.gravitySpeed = 0;
+                    this.gravitySpeed = -5;
                 }
             }
             this.crashWith = function(otherobj) {
@@ -75,6 +75,16 @@
                 }
                 return crash;
             }
+            function updateGameArea() {
+    if (gameobject.crashWith(myObstacle)) {
+        gameobject.stop();
+    } else {
+        myGameArea.clear();
+        myObstacle.update();
+        gameobject.newPos(); 
+        gameobject.update();
+    }
+}
         }
 
         function updateGameArea() {
